9770efe27747eaba0ed0b84af1595ba12e5695a3
Mohamed Hussein <mohamedgobaara@gmail.com>
2024-02-16 15:16:12 +0200

    Initial commit

---
+++ learning/main.c
@@ -0,0 +1,79 @@
+
+//CTRL + K + D  => FORMAT CODE
+
+
+/************************** orginal project   ***************************************/
+
+#define F_CPU 8000000
+#include <util/delay.h>
+
+#include "MemMaph.h"
+#include "StdTypes.h"
+#include "Utils.h"
+#include "SEGMENT_interface.h"
+#include "DIO_interface.h"
+#include "MOTOR_Interface.h"
+#include "LCD_Interface.h"
+#include "STEPPER_Interface.h"
+#include "ADC_interface.h"
+#include "KeyPad_interface.h"
+#include "SENSORS_interface.h"
+#include "EX_INTERRUPT_interface.h"
+#include "KIT.h"
+#include "FireSystem_interface.h"
+#include "Timers_interface.h"
+#include "RGB_LED_interface.h"
+#include "Timer_Services.h"
+#include "ULTRASONIC_interface.h"
+#include "SERVO_interface.h"
+#include "UART_interface.h"
+#include "UART_Services.h"
+#include "AdditionalFun.h"
+#include "UART_STACK.h"
+#include "SPI_interface.h"
+#include "I2C_interface.h"
+#include "WDT.h"
+
+
+#include "I2C_cfg.h"
+#include "I2C_interface.h"
+#include "I2C_Services.h"
+
+
+int main(void)
+{
+	
+// 	/* DIO Initialization */
+// 	DIO_Init() ;
+// 	/* LCD Initialization  */
+// 	LCD_Init()  ;
+// 	/* Global Interrupt enable */
+// 	GLOBAL_INTERRUPT_E ;
+// 	/*EX interrupt Initialization */
+// 	
+// 	//PWM_Init_Timer0()  ;
+// 	//PWM_Freq_HZ_Timer0(50000) ; 
+// 	
+
+
+u8 x  =10   , y =20 ; 
+
+DIO_Init() ; 
+
+u16 z  = x+ y ; 
+
+
+	while(1) {
+		
+		/*************** Start While ********************/
+		
+		/*************** End While ********************/
+	}
+	
+		
+		return   0 ; 	
+		
+	/*************** End Main ********************/
+	
+	
+}
